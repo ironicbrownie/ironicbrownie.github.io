2b8fc3d245b5aec2eee33a825566a63dd5a64ac6
ironicbrownie <81285493+ironicbrownie@users.noreply.github.com>
2021-06-11 18:19:48 +0800

    Update javascript.js

--- website/javascript.js
+++ website/javascript.js
@@ -29,26 +29,3 @@ function openFunction(){
 function closeFunction(){
     document.getElementById("nav-menu").style.width="0px";
 }
-
-
-
-// popup "Added to Cart" window from Sci-fi listing page
-// document.getElementById("cart-icon-2").addEventListener('click', 
-// function(){
-// document.querySelector('.popup').style.display = "flex";
-// });
-
-// document.querySelector('.close').addEventListener('click', 
-// function(){
-//     document.querySelector('.popup').style.display = "none";
-// });
-
-
-// Responsive Nav Menu
-
-// const burger = document.getElementById("burger");
-
-// burger.addEventListener("click", () => {
-//     burger.classList.toggle("active");
-//     console.log(123);
-// });
